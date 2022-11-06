37cf1a6e0ef3f78173ae0de4a3df1492f30ce549
Hellen Wainaina <hellenwain@gmail.com>
2022-11-06 17:37:42 +0300

    Adding dockerfile

--- django_djoser/settings.py
+++ django_djoser/settings.py
@@ -79,10 +79,23 @@ WSGI_APPLICATION = "django_djoser.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
